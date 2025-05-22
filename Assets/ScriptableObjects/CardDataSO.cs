30ce18b593b6dbe381918b061536cb92f8841367
Vulji <120482641+Vulji@users.noreply.github.com>
2025-05-22 20:07:20 +0200

    Finalization of the card dataset foundations
    Completed the card type SO enum. Added an ID + refactorization of the carddataSO script for better clarity. Added a DataBaseSO script to gather all the card data in one place + some helper methods inside it.

--- Assets/ScriptableObjects/CardDataSO.cs
+++ Assets/ScriptableObjects/CardDataSO.cs
@@ -6,34 +6,38 @@ using Sirenix.Serialization;
 public class CardDataSO : ScriptableObject
 {
     [Title("Card Visual")]
-    [HorizontalGroup("CardData", 75)]
-    [PreviewField(70, ObjectFieldAlignment.Left)]
-    [HideLabel]
+    [HorizontalGroup("CardData", 75), VerticalGroup("CardData/Left", PaddingBottom = 5)]
+    [PreviewField(70, ObjectFieldAlignment.Left), HideLabel]
     public GameObject CardModel;
 
     [Title("Card Stats")]
-    [VerticalGroup("CardData/Stats", 10)]
-    [LabelWidth(75)]
+    [VerticalGroup("CardData/Stats", 10), LabelWidth(75)]
     public string Name;
     
-    [VerticalGroup("CardData/Stats")]
-    [LabelWidth(75)]
+    [VerticalGroup("CardData/Stats"),LabelWidth(75)]
     public CardTypeSO CardType;
 
-    [VerticalGroup("CardData/Stats")]
-    [LabelWidth(120)]
-    [Range(0, 100), Unit(Units.Percent)]
+    [VerticalGroup("CardData/Stats"), LabelWidth(120), Range(0, 100), Unit(Units.Percent)]
     public float AppearanceChance;
 
-    [VerticalGroup("CardData/Stats")]
-    [LabelWidth(120)]
-    [Range(0, 10)]
+    [VerticalGroup("CardData/Stats"), LabelWidth(120), Range(0, 10)]
     public int ActionPointCost;
 
-    [VerticalGroup("CardData/Stats")]
-    [LabelWidth(120)]
-    [Range(0, 10)]
+    [VerticalGroup("CardData/Stats"), LabelWidth(120), Range(0, 10)]
     public int Value;
 
+    [VerticalGroup("CardData/Left"), LabelWidth(200), MinValue(0), MaxValue(15)]
+    [ShowIf("Toggle"),HideLabel]
+    public int ID;
+    
+    private string _buttonName = "ID";
+    [HideInInspector]
+    public bool Toggle;
 
+    [VerticalGroup("CardData/Left")]
+    [Button("$_buttonName", ButtonSizes.Small, Stretch = false, ButtonAlignment = 0.5f)]
+    private void IDButton()
+    {
+        this.Toggle = !this.Toggle;
+    }
 }
