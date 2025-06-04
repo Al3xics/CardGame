2874fb86825352a299270e8e3cc9acfd624e62b0
Alexis <alexis.martinho.78@gmail.com>
2025-06-04 11:43:53 +0200

    State Machine

---
+++ Assets/Scripts/FSM/State.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Wendogo
+{
+    public abstract class State<T> : IState where T : StateMachine<T>
+    {
+        protected T StateMachine;
+
+        protected State(T stateMachine)
+        {
+            StateMachine = stateMachine;
+        }
+        
+        public virtual void OnEnter()
+        {
+            Debug.Log($"Enter {GetType()}");
+        }
+
+        public virtual void OnTick()
+        {
+            Debug.Log($"Tick {GetType()}");
+        }
+
+        public virtual void OnExit()
+        {
+            Debug.Log($"Exit {GetType()}");
+        }
+    }
+}
