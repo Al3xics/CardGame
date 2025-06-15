4a2b1a79148679920a50285fc47555486269db05
Phylip3 <martin.revolbuisson@gmail.com>
2025-06-15 13:22:47 +0200

    Comment ServerManager

--- Assets/PROG/Scripts/Server/ServerManager.cs
+++ Assets/PROG/Scripts/Server/ServerManager.cs
@@ -17,25 +17,23 @@ namespace Wendogo
         public event Action OnDrawCard;
         public event Action OnPlayerTurnEnded;
         public event Action OnNightConsequencesEnded;
-        public string gameSceneName = "Game";
+        public string GameSceneName { get; private set; } = "Game";
         private Dictionary<ulong, PlayerController> _playersById;
 
         #endregion
 
+        // Fonction appelée lors de l'initialisation du script pour configurer l'instance du ServerManager
+        // et désactiver certains objets pour les clients (non-serveur).
         private void Awake()
         {
             if (Instance == null)
             {
                 Instance = this;
                 DontDestroyOnLoad(gameObject);
-
-                if (AutoSessionBootstrapper.AutoConnect)
-                {
-                    // Disable GameStateMachine for clients because only the host can do things with it
-                    GameObject gameStateMachineObject = GameObject.Find("GameStateMachine");
-                    if (!IsServer && gameStateMachineObject)
-                        gameStateMachineObject.SetActive(false);
-                }
+                // Désactive GameStateMachine pour les Clients car seul celui qui host la partie peut exectuter ce code
+                GameObject gameStateMachineObject = GameObject.Find("GameStateMachine");
+                if (!IsServer && gameStateMachineObject)
+                    gameStateMachineObject.SetActive(false);
             }
             else
             {
@@ -43,6 +41,9 @@ namespace Wendogo
             }
         }
 
+        // Initialise le dictionnaire des joueurs présents dans la scène,
+        // en associant leur ID réseau à leur PlayerController respectif.
+        // Enregistre aussi chaque joueur dans la GameStateMachine.
         public void InitializePlayers()
         {
             _playersById = FindObjectsByType<PlayerController>(FindObjectsSortMode.None).ToDictionary(p => p.OwnerClientId);
@@ -52,17 +53,28 @@ namespace Wendogo
                 GameStateMachine.Instance.RegisterPlayerID(player.OwnerClientId);
             }
 
-            if (!AutoSessionBootstrapper.AutoConnect)
-            {
-                // Disable GameStateMachine for clients because only the host can do things with it
-                GameObject gameStateMachineObject = GameObject.Find("GameStateMachine");
-                if (!IsServer && gameStateMachineObject)
-                    gameStateMachineObject.SetActive(false);
-            }
+            /*// Disable GameStateMachine for clients because only the host can do things with it
+            GameObject gameStateMachineObject = GameObject.Find("GameStateMachine");
+            if (!IsServer && gameStateMachineObject)
+                gameStateMachineObject.SetActive(false);*/
+        }
+
+        // Lance le début du jeu en activant la GameStateMachine (réservé à l'hôte).
+        public void StartGame()
+        {
+            Debug.Log("All players are ready. Starting GameStateMachine...");
+            // Désactive GameStateMachine pour les Clients car seul celui qui host la partie peut exectuter ce code
+            GameObject gameStateMachineObject = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(go => go.name == "GameStateMachine");
+
+            if (gameStateMachineObject != null)
+                gameStateMachineObject.SetActive(true);
         }
 
         #region RPC
 
+
+        // Attribue les rôles aux joueurs en fonction de leur ID. Fonction appelée côté serveur.
+        // Chaque joueur reçoit son rôle via un appel ClientRpc.
         [ServerRpc(RequireOwnership = false)]
         public void AssignRolesToPlayersServerRpc(ulong[] clientIds, RoleType[] roles, ServerRpcParams rpcParams = default)
         {
@@ -80,6 +92,9 @@ namespace Wendogo
             OnAssignedRoles?.Invoke();
         }
 
+
+        // Envoie les cartes tirées à chaque joueur depuis le serveur.
+        // Chaque joueur reçoit ses cartes via un appel ClientRpc.
         [ServerRpc(RequireOwnership = false)]
         public void SendCardsToPlayersServerRpc(ulong[] target, int[][] intArray, ServerRpcParams rpcParams = default)
         {
@@ -96,6 +111,8 @@ namespace Wendogo
             OnDrawCard?.Invoke();
         }
 
+
+        // Démarre le tour du joueur spécifié en appelant un ClientRpc côté client.
         [ServerRpc(RequireOwnership = false)]
         public void PlayerTurnServerServerRpc(ulong playerId)
         {
@@ -105,12 +122,17 @@ namespace Wendogo
             }
         }
 
+
+        // Notifie que le tour d’un joueur est terminé en déclenchant l’événement approprié.
         [ServerRpc(RequireOwnership = false)]
         public void PlayerTurnEndedServerRpc()
         {
             OnPlayerTurnEnded?.Invoke();
         }
 
+
+        // Demande au serveur de faire piocher des cartes à la StateMachine pour un joueur spécifique à partir d’un deck spécifique.
+        // Utilisé si des cartes manquent (ex. : après une utilisation).
         [ServerRpc(RequireOwnership = false)]
         public void TransmitMissingCardsServerRpc(int drawXCardsFromDeck, int deckID, ServerRpcParams rpcParams = default)
         {
@@ -119,24 +141,38 @@ namespace Wendogo
         }
 
         #endregion
-        
+
 
         #region Basic Methodes
 
+
+        // Lance le chargement de la scène de jeu depuis le serveur si elle n’est pas déjà active.
         public void LaunchGame()
         {
-            if (IsServer && SceneManager.GetActiveScene().name != gameSceneName)
-                NetworkManager.SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+            if (IsServer && SceneManager.GetActiveScene().name != GameSceneName)
+                NetworkManager.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
+        }
+
+
+        // Récupère et retourne le nom du joueur actuel à partir des propriétés de la session.
+        public string GetPlayerName()
+        {
+            if (SessionManager.Instance.ActiveSession.CurrentPlayer.Properties.TryGetValue(SessionConstants.PlayerNamePropertyKey, out var playerName))
+                return playerName.Value;
+
+            return "Inconnu";
         }
 
         #endregion
 
-
-        public void ChangePlayersHealth(Dictionary<ulong,int> playersHealth)
+        // Prévue pour modifier les points de vie des joueurs selon un dictionnaire ID / santé.
+        public void ChangePlayersHealth(Dictionary<ulong, int> playersHealth)
         {
 
         }
 
+
+        // Fonction vide prévue pour signaler la fin de la vérification d'une carte jouée.
         public void FinishedCheckCardPlayed()
         {
 
@@ -149,6 +185,7 @@ namespace Wendogo
         }*/
 
 
+        // jsp
         [ServerRpc(RequireOwnership = false)]
         public void SendDataServerServerRpc(ServerRpcParams rpcParams = default)
         {
