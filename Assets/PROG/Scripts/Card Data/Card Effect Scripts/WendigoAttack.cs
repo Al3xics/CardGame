5edcbe7053be4128ebd86a66ece7474a627b5fed
Phylip3 <martin.revolbuisson@gmail.com>
2025-06-30 15:37:54 +0200

    Add SelectRessourceCanvas prefab to UI Prefabs folder for resource selection functionality

--- Assets/PROG/Scripts/Card Data/Card Effect Scripts/WendigoAttack.cs
+++ Assets/PROG/Scripts/Card Data/Card Effect Scripts/WendigoAttack.cs
@@ -8,15 +8,7 @@ namespace Wendogo
         public int damageDone = 1;
         public override void Apply(ulong owner, ulong target, int value = -1)
         {
-            // If 'value' is '-1', then no value was passed and just use foodGained
-            int damage = value != -1 ? value + damageDone : damageDone;
-            var player = PlayerController.GetPlayer(target);
-            Debug.Log($"Scavenge food by {damage}");
             
-            if (player.IsSimulatingNight)
-                player.hiddenHealth -= damage; // Night, so apply only for local player
-            else
-                player.health.Value -= damage; // Day, so apply for all players
         }
     }
 }
