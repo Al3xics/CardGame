a151a7819bfff6c0194cc26a02fbaad5370a972a
Phylip3 <martin.revolbuisson@gmail.com>
2025-07-09 18:31:51 +0200

    Add BloodBounty passive effect: boost damage for specific card effects based on player role and card type

--- Assets/PROG/Scripts/Card Data/Card Effect Scripts/BloodBounty.cs
+++ Assets/PROG/Scripts/Card Data/Card Effect Scripts/BloodBounty.cs
@@ -5,6 +5,23 @@ namespace Wendogo
     [CreateAssetMenu(fileName = "BloodBounty", menuName = "Card Effects/Blood Bounty")]
     public class BloodBounty : CardEffect
     {
-
+        public int boostDamage = 1;
+        public override bool ApplyPassive(int playedCardId, ulong origin, ulong target, out int value)
+        {
+            value = -1;
+            var card = DataCollection.Instance.cardDatabase.GetCardByID(playedCardId);
+            
+            PlayerController originPlayer = PlayerController.GetPlayer(origin);
+            
+            if (card.CardEffect is WendigoAttack || (card.CardEffect is FightOrFlight && originPlayer.Role.Value == RoleType.Wendogo) || card.CardEffect is GroupAttack)
+            {
+                value = boostDamage;
+                Debug.Log($"Attack boosted by {value}");
+                return true;
+            }
+            
+            value = -1;
+            return false;
+        }
     }
 }
