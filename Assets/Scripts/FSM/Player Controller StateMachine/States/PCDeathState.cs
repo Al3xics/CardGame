6d580bd3595507fe156a845d0fd591777d9390f2
Alexis <alexis.martinho.78@gmail.com>
2025-08-10 15:21:09 +0200

    PCDeathState creation

---
+++ Assets/Scripts/FSM/Player Controller StateMachine/States/PCDeathState.cs
@@ -0,0 +1,11 @@
+﻿namespace Wendogo
+{
+    public class PCDeathState : State<PlayerControllerSM>
+    {
+        private PlayerController _player;
+        
+        public PCDeathState(PlayerControllerSM stateMachine, PlayerController player) : base(stateMachine) { _player = player; }
+        
+        
+    }
+}
