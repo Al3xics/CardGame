cb6bad2fb4db4c7407a8cbee278bcbfc7afa3cf0
Vulji <valentin.luzolo@protonmail.com>
2025-08-11 21:31:58 +0200

    Silence card done

--- Assets/Scripts/Card Data/Card Effect Scripts/Silence.cs
+++ Assets/Scripts/Card Data/Card Effect Scripts/Silence.cs
@@ -10,18 +10,22 @@ namespace Wendogo
         public override void Apply(ulong owner, ulong target, int value = -1)
         {
             ServerManager.Instance.MutePlayerRpc(true, target);
-            
+            Debug.Log("Silenced");
             AnalyticsManager.Instance.RecordEvent(new CustomEvent("silenceActiveCardWasApplied"));
         }
         
         public override void ShowUI()
         {
-
+            if (prefabUI == null)
+                prefabUI = FindAnyObjectByType<CanvaTarget>(FindObjectsInactive.Include).gameObject;
+            prefabUI.SetActive(true);
         }
 
         public override void HideUI(bool clearVotes)
         {
-
+            if (prefabUI == null)
+                prefabUI = FindAnyObjectByType<CanvaTarget>(FindObjectsInactive.Include).gameObject;
+            prefabUI.SetActive(false);
             base.HideUI(clearVotes);
         }
     }
