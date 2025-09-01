aea9e063db3983c86b7be76810f3c185ac9d979c
Vulji <valentin.luzolo@protonmail.com>
2025-09-01 18:41:11 +0200

    Added Ritual gauge

--- Assets/Scripts/UI/RitualUI.cs
+++ Assets/Scripts/UI/RitualUI.cs
@@ -7,4 +7,6 @@ public class RitualUI : MonoBehaviour
     [SerializeField] public TextMeshProUGUI woodUI;
     [SerializeField] public TextMeshProUGUI foodUI;
     [SerializeField] public List<GameObject> ritualParts = new List<GameObject>();
+    [SerializeField] public List<GameObject> foodGaugeParts = new List<GameObject>();
+    [SerializeField] public List<GameObject> woodGaugeParts = new List<GameObject>();
 }
