3421115a8c0c6341a8b5915e970855ff7258baea
Phylip3 <martin.revolbuisson@gmail.com>
2025-09-03 03:32:52 +0200

    Update `DarkTrick` card effect: refine UI logic, dynamically display player names and icons using `ServerManager.GlobalPlayersByName` and `PlayerUI`.

--- Assets/Scripts/Card Data/Card Effect Scripts/DarkTrick.cs
+++ Assets/Scripts/Card Data/Card Effect Scripts/DarkTrick.cs
@@ -28,6 +28,21 @@ namespace Wendogo
                 base.ShowUI(_dtCanvaInstance);
                 _dtCanvaInstance.SetActive(true);
                 
+                ServerManager.GlobalPlayersByName.TryGetValue(0, out var value1);
+                ServerManager.GlobalPlayersByName.TryGetValue(1, out var value2);
+                ServerManager.GlobalPlayersByName.TryGetValue(2, out var value3);
+                ServerManager.GlobalPlayersByName.TryGetValue(3, out var value4);
+            
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer1").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value1;
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer2").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value2;
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer3").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value3;
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer4").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value4;
+            
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer1").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[0];
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer2").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[1];
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer3").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[2];
+                _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer4").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[3];
+                
                 
             }
             else
@@ -36,22 +51,23 @@ namespace Wendogo
                     _dtCanvaInstance = Instantiate(SelectTargetPrefabUI);
                 base.ShowUI(_dtCanvaInstance);
                 _dtCanvaInstance.SetActive(true);
-            }
-            
-            ServerManager.GlobalPlayersByName.TryGetValue(0, out var value1);
+                
+                ServerManager.GlobalPlayersByName.TryGetValue(0, out var value1);
             ServerManager.GlobalPlayersByName.TryGetValue(1, out var value2);
             ServerManager.GlobalPlayersByName.TryGetValue(2, out var value3);
             ServerManager.GlobalPlayersByName.TryGetValue(3, out var value4);
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer1").transform.Find("Player_2").transform.Find("Player_2_Name").GetComponent<TMP_Text>().text = value1;
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer2").transform.Find("Player_2").transform.Find("Player_2_Name").GetComponent<TMP_Text>().text = value2;
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer3").transform.Find("Player_2").transform.Find("Player_2_Name").GetComponent<TMP_Text>().text = value3;
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer4").transform.Find("Player_2").transform.Find("Player_2_Name").GetComponent<TMP_Text>().text = value4;
+                
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer1").transform.Find("Player_2").transform.Find("Player_2_Icon").GetComponent<RawImage>().texture = PlayerUI.Instance.PlayerImageIcons[0];
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer2").transform.Find("Player_2").transform.Find("Player_2_Icon").GetComponent<RawImage>().texture = PlayerUI.Instance.PlayerImageIcons[1];
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer3").transform.Find("Player_2").transform.Find("Player_2_Icon").GetComponent<RawImage>().texture = PlayerUI.Instance.PlayerImageIcons[2];
+            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer4").transform.Find("Player_2").transform.Find("Player_2_Icon").GetComponent<RawImage>().texture = PlayerUI.Instance.PlayerImageIcons[3];
+            }
             
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer1").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value1;
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer2").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value2;
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer3").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value3;
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer4").transform.Find("Text (TMP)").GetComponent<TMP_Text>().text = value4;
             
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer1").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[0];
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer2").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[1];
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer3").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[2];
-            _dtCanvaInstance.transform.Find("UpdatedSelectTargetCanvas").transform.Find("Panel").transform.Find("ButtonPlayer4").GetComponent<Image>().sprite = PlayerUI.Instance.PlayerSpritesIcons[3];
         }
 
         public override void HideUI(bool clearVotes, GameObject uiInstance = null)
